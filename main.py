f1ad16bba716ab8ad4f7a66d8fcadaa172241327
Samuel S <kongsamuel@hotmail.dk>
2020-11-12 15:46:59 +0100

    Some sample code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Raspberry Pi Project
+from grove.factory import Factory
+
+run = True
+pin = 12
+button = Factory.getButton("GPIO-HIGH", pin)
+
+while run:
+    if button.is_pressed():
+        print('Button is pressed')
+    else:
+        print('Button is released')
