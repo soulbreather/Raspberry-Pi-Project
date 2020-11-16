0d66086338049cfd5353dacd3708b9aeefab2fd5
Samuel S <kongsamuel@hotmail.dk>
2020-11-16 10:16:43 +0100

    more fixes and code

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # Raspberry Pi Project
-from grovepi import *
+#from grovepi import *
 import time
 import getNews
+import pyttsx3
 
 engine = pyttsx3.init()
 run = True
-button = 3
+button1 = 3
+button2 = 4
+realNews = getNews.getNewsHeadline()
+fakeNews = getNews.getFakeNewsHeadline()
+
+print(realNews)
+print(fakeNews)
 
 print("fake or not game is running")
 
 while True:
-    if(digitalRead(button)):
-        time.sleep(0.1)
-        print("button was pressed")
-        engine.say(getNews)
+    if(digitalRead(button1)):
+        time.sleep(0.01)
+        print("button 1 was pressed")
+        engine.say(realNews)
+        engine.runAndWait()
+
+    if(digitalRead(button2)):
+        time.sleep(0.01)
+        print("button 2 was pressed")
+        engine.say(fakeNews)
         engine.runAndWait()
